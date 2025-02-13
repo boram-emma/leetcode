904d164a98b3387c0f6e0de26ee0880c11633630
boramemma <boramlee0418@gmail.com>
2025-02-12 20:19:17 -0500

    Time: 887 ms (98.34%), Space: 0B (100%) - LeetHub

---
+++ 0577-employee-bonus/0577-employee-bonus.sql
@@ -0,0 +1,5 @@
+# Write your MySQL query statement below
+select e.name as name, coalesce(b.bonus, NULL) as bonus
+from Employee e
+left join Bonus b on e.empId = b.empId
+where b.bonus < 1000 or b.bonus is NULL
