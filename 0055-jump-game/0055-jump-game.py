656b14c644beea60af92bdf24c976fc640f495c8
boramemma <boramlee0418@gmail.com>
2025-02-16 15:58:51 -0500

    Time: 4 ms (99.1%), Space: 18.6 MB (52.85%) - LeetHub

--- 0055-jump-game/0055-jump-game.py
+++ 0055-jump-game/0055-jump-game.py
@@ -1,15 +1,9 @@
-from typing import List
-
 class Solution:
     def canJump(self, nums: List[int]) -> bool:
-        max_reach = 0  # 현재 도달 가능한 최대 인덱스
         
-        for i, jump in enumerate(nums):
-            if i > max_reach:  # 현재 위치가 도달할 수 없는 곳이라면 False
-                return False
-            max_reach = max(max_reach, i + jump)  # 최대 도달 가능 위치 갱신
-            
-            if max_reach >= len(nums) - 1:  # 마지막 인덱스에 도달 가능하면 True
-                return True
+        goal = len(nums)-1
+        for i in range(len(nums)-1,-1,-1):
+            if i + nums[i] >= goal:
+                goal = i
         
-        return False  # 마지막까지 도달 못하면 False
+        return True if goal == 0 else False
