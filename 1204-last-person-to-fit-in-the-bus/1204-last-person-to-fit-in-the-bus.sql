87aa0cb19bffb14f8cf5be3c3f71e5590b2da11a
boramemma <boramlee0418@gmail.com>
2025-02-23 19:55:18 -0500

    Time: 1125 ms (86.4%), Space: 0B (100%) - LeetHub

--- 1204-last-person-to-fit-in-the-bus/1204-last-person-to-fit-in-the-bus.sql
+++ 1204-last-person-to-fit-in-the-bus/1204-last-person-to-fit-in-the-bus.sql
@@ -1,11 +1,24 @@
 # Write your MySQL query statement below
-WITH boarding AS (
-    SELECT person_name, turn, 
-           SUM(weight) OVER (ORDER BY turn) AS total_weight
+-- WITH boarding AS (
+--     SELECT person_name, turn, 
+--            SUM(weight) OVER (ORDER BY turn) AS total_weight
+--     FROM Queue
+-- )
+-- SELECT person_name
+-- FROM boarding
+-- WHERE total_weight <= 1000
+-- ORDER BY total_weight DESC
+-- LIMIT 1;
+
+
+WITH cumulQue AS (
+    SELECT *,
+            SUM(weight) OVER (ORDER BY turn) AS cumul
     FROM Queue
 )
-SELECT person_name
-FROM boarding
-WHERE total_weight <= 1000
-ORDER BY total_weight DESC
+
+SELECT c.person_name AS person_name
+FROM cumulQue c
+WHERE c.cumul <= 1000
+ORDER BY c.cumul DESC
 LIMIT 1;
